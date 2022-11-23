9584c6385e090aa12f66193b2542f67d68a5d6b5
Yves Paychere <yves.paychere@epfl.ch>
2022-11-23 13:16:49 +0100

    modified to have the threshold that depends on the percentils

--- scripts/make_artefact_labels.py
+++ scripts/make_artefact_labels.py
@@ -5,7 +5,7 @@ from tifffile import imwrite
 import scipy.ndimage as ndimage
 
 
-def make_artefact_labels(image, labels, artefact_threshold_adjustment=0):
+def make_artefact_labels(image, labels, threshold_artefact_brightness_percent=50,threshold_artefact_size_percent=50):
     """Make a new label image with artefacts labelled as 1.
     Parameters
     ----------
@@ -13,8 +13,10 @@ def make_artefact_labels(image, labels, artefact_threshold_adjustment=0):
         Image data.
     labels : ndarray
         Label image data.
-    artefact_threshold_adjustment : float, optional
-        Threshold adjustment parameter for artefact detection, it will multiply the standard deviation of the pixels value, by default 0
+    threshold_artefact_brightness_percent : int, optional
+        The artefacts need to be as least as bright as this percentage of the neurone's pixels.
+    threshold_artefact_size : int, optional
+        The artefacts need to be at least as big as this percentage of the neurones.
     Returns
     -------
     ndarray
@@ -23,33 +25,24 @@ def make_artefact_labels(image, labels, artefact_threshold_adjustment=0):
     neurones = np.array(labels > 0)
     non_neurones = np.array(labels == 0)
 
-    # calculate mean intensity of all the pixels that are labeled as neurones
-    mean = np.mean(image[neurones])
 
-    # calculate standard deviation of the intensity of all the pixels that are labeled as neurones
-    std = np.std(image[neurones])
-
-    # calculate the threshold
-    threshold = mean + artefact_threshold_adjustment * std
-    # print("threshold", threshold, "mean", mean, "std", std)
+    # calculate the percentile of the intensity of all the pixels that are labeled as neurones
+    threshold = np.percentile(image[neurones], threshold_artefact_brightness_percent)
 
     if threshold < 0:
         threshold = 0
-        print("Warning: threshold for artefact detection is negative, setting to 0")
 
     # take all the pixels that are above the threshold and that are not labeled as neurones
     artefacts = np.where(image > threshold, 1, 0)
     artefacts = np.where(non_neurones, artefacts, 0)
 
-    # calculate the mean size of the neurones
-    neurone_size = np.mean(ndimage.sum(neurones, labels, range(np.max(labels) + 1)))
-    # calculate the standard deviation of the neurone size
-    neurone_size_std = np.std(ndimage.sum(neurones, labels, range(np.max(labels) + 1)))
+    #calculate the percentile of the size of the neurones
+    neurone_size_percentile = np.percentile(ndimage.sum(neurones, labels, range(np.max(labels) + 1)), threshold_artefact_size_percent)
     # print("mean neurone size: ", neurone_size, "std: ", neurone_size_std)
 
     # select artefacts by size
     artefacts = select_artefacts_by_size(
-        artefacts, neurone_size - 0.5 * neurone_size_std
+        artefacts, neurone_size_percentile
     )
 
     return artefacts
@@ -66,7 +59,7 @@ def select_artefacts_by_size(artefacts, min_size):
     Returns
     -------
     ndarray
-        Label image with artefacts labelled as 1 and small artefacts removed.
+        Label image with artefacts labelled and small artefacts removed.
     """
     # find all the connected components in the artefacts image
     labels, nlabels = ndimage.label(artefacts)
@@ -95,7 +88,7 @@ image = imread(im)
 labeled_im = "dataset/visual_tif/labels/testing_im.tif"
 labels = imread(labeled_im)
 
-artefacts = make_artefact_labels(image, labels, artefact_threshold_adjustment=-1)
+artefacts = make_artefact_labels(image, labels, threshold_artefact_brightness_percent=20,threshold_artefact_size_percent=10)
 
 # save the artefact image
 imwrite("dataset/visual_tif/artefact.tif", artefacts)
