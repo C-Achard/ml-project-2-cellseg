824e5f71467f85f428e2f417020039d3f584e763
Yves Paychere <yves.paychere@epfl.ch>
2022-11-29 11:09:16 +0100

    refactor the function, added a visualization, better handling of the case without neurones

--- scripts/make_artefact_labels.py
+++ scripts/make_artefact_labels.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from tifffile import imread
 from tifffile import imwrite
 import scipy.ndimage as ndimage
 import os
+import napari
 
 
 def make_artefact_labels(
@@ -43,18 +43,25 @@ def make_artefact_labels(
         threshold = np.percentile(image[neurones], threshold_artefact_brightness_percent)
     else:
         #take the percentile of the non neurones if the neurones are empty
-        threshold = np.percentile(image[non_neurones], threshold_artefact_brightness_percent)
+        threshold = np.percentile(image[non_neurones], 90)
 
     # take all the pixels that are above the threshold and that are not labeled as neurones
     artefacts = np.where(image > threshold, 1, 0)
     artefacts = np.where(non_neurones, artefacts, 0)
 
     # calculate the percentile of the size of the neurones
-    neurone_size_percentile = np.percentile(
-        ndimage.sum(neurones, labels, range(np.max(labels) + 1)),
-        threshold_artefact_size_percent,
-    )
-    # print("mean neurone size: ", neurone_size, "std: ", neurone_size_std)
+    if np.sum(neurones) > 0:
+        neurone_size_percentile = np.percentile(
+            ndimage.sum(neurones, labels, range(np.max(labels) + 1)),
+            threshold_artefact_size_percent,
+        )
+    else:
+        # find all the connected components in the artefacts image
+        labels, nlabels = ndimage.label(artefacts)
+        # find the size of each connected component
+        sizes = ndimage.sum(artefacts, labels, range(nlabels + 1))
+        #remove the 50% smallest connected components
+        neurone_size_percentile = np.percentile(sizes, 95)
 
     # select artefacts by size
     artefacts = select_artefacts_by_size(artefacts, neurone_size_percentile)
@@ -133,7 +140,52 @@ def create_artefact_labels(
 
     imwrite(output_path, neurones_artefacts_labels)
 
-# load the dataset
+def visualize_images(paths):
+    """Visualize images.
+    Parameters
+    ----------
+    paths : list
+        List of paths to images to visualize.
+    """
+    viewer = napari.Viewer()
+    for path in paths:
+        viewer.add_image(imread(path), name=os.path.basename(path))
+    #wait for the user to close the viewer
+    napari.run()
+
+def create_artefact_labels_from_folder(
+    path,
+    do_visualize=False,
+    threshold_artefact_brightness_percent=20,
+    threshold_artefact_size_percent=10,
+):
+    """Create a new label image with artefacts labelled as 2 and neurones labelled as 1 for all images in a folder. The images created are stored in a folder artefact_neurones.
+    Parameters
+    ----------
+    path : str
+        Path to folder with images in folder volumes and labels in folder lab_sem. The images are expected to have the same alphabetical order in both folders.
+    do_visualize : bool, optional
+        If True, the images will be visualized.
+    threshold_artefact_brightness_percent : int, optional
+        The artefacts need to be as least as bright as this percentage of the neurone's pixels.
+    threshold_artefact_size : int, optional
+        The artefacts need to be at least as big as this percentage of the neurones.
+    """
+    #find all the images in the folder and create a list
+    path_labels = [f for f in os.listdir(path+"/lab_sem") if f.endswith(".tif")]
+    path_images = [f for f in os.listdir(path+"/volumes") if f.endswith(".tif")]
+    #sort the list
+    path_labels.sort()
+    path_images.sort()
+    #create the output folder
+    os.makedirs(path+"/artefact_neurones", exist_ok=True)
+    #create the artefact labels
+    for i in range(len(path_labels)):
+        print(path_labels[i])
+        #consider that the images and the labels have names in the same alphabetical order
+        create_artefact_labels(path+"/volumes/"+path_images[i], path+"/lab_sem/"+path_labels[i], path+"/artefact_neurones/"+path_labels[i], threshold_artefact_brightness_percent, threshold_artefact_size_percent)
+        if do_visualize:
+            visualize_images([path+"/volumes/"+path_images[i], path+"/lab_sem/"+path_labels[i], path+"/artefact_neurones/"+path_labels[i]])
 
 """
 
@@ -164,16 +216,8 @@ paths=["dataset/cropped_visual/train","dataset/cropped_visual/val","dataset/soma
 
 for path in paths:
     print(path)
-    #find all the images in the folder and create a list
-    path_labels = [f for f in os.listdir(path+"/lab_sem") if f.endswith(".tif")]
-    path_images = [f for f in os.listdir(path+"/volumes") if f.endswith(".tif")]
-    #sort the list
-    path_labels.sort()
-    path_images.sort()
-    #create the output folder
-    os.makedirs(path+"/artefact_neurones", exist_ok=True)
-    #create the artefact labels
-    for i in range(len(path_labels)):
-        print(path_labels[i])
-        #consider that the images and the labels have names in the same alphabetical order
-        create_artefact_labels(path+"/volumes/"+path_images[i], path+"/lab_sem/"+path_labels[i], path+"/artefact_neurones/"+path_labels[i])
+    create_artefact_labels_from_folder(path, do_visualize=False)
+
+
+#create_artefact_labels("dataset/somatomotor/volumes/c3images.tif", "dataset/somatomotor/lab_sem/c3labels.tif", "dataset/somatomotor/artefact_neurones/c3labels.tif", threshold_artefact_brightness_percent=20, threshold_artefact_size_percent=2)
+#visualize_images(["dataset/somatomotor/volumes/c3images.tif", "dataset/somatomotor/lab_sem/c3labels.tif", "dataset/somatomotor/artefact_neurones/c3labels.tif"])
