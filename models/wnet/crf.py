2e1f2d5bdf4743aa1d51c6d3fe7cf0ba59df72a1
Colin Hofmann <colin.hofmann@epfl.ch>
2022-12-18 15:18:05 +0100

    black

--- models/wnet/crf.py
+++ models/wnet/crf.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pydensecrf.densecrf as dcrf
-from pydensecrf.utils import unary_from_softmax, create_pairwise_gaussian, create_pairwise_bilateral
+from pydensecrf.utils import (
+    unary_from_softmax,
+    create_pairwise_gaussian,
+    create_pairwise_bilateral,
+)
+
 
 def crf_batch(images, probs, sa, sb, sg, n_iter=5):
-    """ CRF post-processing step for the W-Net, applied to a batch of images.
+    """CRF post-processing step for the W-Net, applied to a batch of images.
 
     Args:
         images (np.ndarray): Array of shape (N, C, H, W, D) containing the input images.
@@ -15,11 +20,18 @@ def crf_batch(images, probs, sa, sb, sg, n_iter=5):
     Returns:
         np.ndarray: Array of shape (N, K, H, W, D) containing the refined class probabilities for each pixel.
     """
-    
-    return np.stack([crf(images[i], probs[i], sa, sb, sg, n_iter=n_iter) for i in range(images.shape[0])], axis=0)
+
+    return np.stack(
+        [
+            crf(images[i], probs[i], sa, sb, sg, n_iter=n_iter)
+            for i in range(images.shape[0])
+        ],
+        axis=0,
+    )
+
 
 def crf(image, prob, sa, sb, sg, n_iter=5):
-    """ Implements the CRF post-processing step for the W-Net.
+    """Implements the CRF post-processing step for the W-Net.
     Inspired by https://arxiv.org/abs/1210.5644, https://arxiv.org/abs/1606.00915 and https://arxiv.org/abs/1711.08506.
     Implemented using the pydensecrf library.
 
@@ -41,14 +53,23 @@ def crf(image, prob, sa, sb, sg, n_iter=5):
 
     # Generate pairwise potentials
     featsGaussian = create_pairwise_gaussian(sdims=(sg, sg, sg), shape=image.shape[1:])
-    featsBilateral = create_pairwise_bilateral(sdims=(sa, sa, sa), schan=[sb for i in range(image.shape[0])], img=image, chdim=image.shape[0])
+    featsBilateral = create_pairwise_bilateral(
+        sdims=(sa, sa, sa),
+        schan=[sb for i in range(image.shape[0])],
+        img=image,
+        chdim=image.shape[0],
+    )
 
     # Add pairwise potentials to the CRF
-    compat = np.ones(prob.shape[0], dtype=np.float32) - np.diag([1 for i in range(prob.shape[0])], dtype=np.float32)
+    compat = np.ones(prob.shape[0], dtype=np.float32) - np.diag(
+        [1 for i in range(prob.shape[0])], dtype=np.float32
+    )
     d.addPairwiseEnergy(featsGaussian, compat=compat)
     d.addPairwiseEnergy(featsBilateral, compat=compat)
 
     # Run inference
     Q = d.inference(n_iter)
 
-    return np.array(Q).reshape((prob.shape[0], image.shape[1], image.shape[2], image.shape[3]))
+    return np.array(Q).reshape(
+        (prob.shape[0], image.shape[1], image.shape[2], image.shape[3])
+    )
